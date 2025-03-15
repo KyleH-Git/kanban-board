638f971ec8f6cb383661691a9b9625840a630d42
Kyle Hayden <dahaydnator@gmail.com>
2025-03-14 21:10:46 -0500

    broke clearModal into its own function, cleaned up newPost, cancelPostBtn, & newSubmitBtn eventListeners, added logic for clearing modals

--- js/script.js
+++ js/script.js
@@ -3,26 +3,53 @@ const newSubmitBtn = document.querySelector('.submitBtn');
 const cancelPostBtn = document.querySelector('.cancelBtn')
 const postModalElement = document.querySelector('.post-modal');
 
+
+const clearModal = () =>{
+    document.querySelector('#postTitle').value = '';
+    document.querySelector('#postBody').value = '';
+    document.querySelector('.modal-error').style.display = 'none';
+    postModalElement.style.display = 'none';
+}
+
+const postDrag = () =>{
+
+}
+
 newPostBtn.addEventListener('click', () => {
+    if(postModalElement.style.display == 'flex'){
+        clearModal();
+        return;
+    }
     postModalElement.style.display = "flex";
+
 });
 
-cancelPostBtn.addEventListener('click', () => {
-    postModalElement.style.display = "none";
-});
+cancelPostBtn.addEventListener('click', clearModal);
 
-newSubmitBtn.addEventListener('click', () =>{
+newSubmitBtn.addEventListener('click', (event) =>{
     event.preventDefault();
     const newPost = document.createElement('div');
     const newPostTitle = document.createElement('div');
-    newPostTitle.innerText = document.querySelector('#postTitle').value;
     const newPostBody = document.createElement('div');
-    newPostBody.innerText = document.querySelector('#postBody').value;
+    const userTitle = document.querySelector('#postTitle').value;
+    const userBody = document.querySelector('#postBody').value;
+    if(userTitle){
+        newPostTitle.innerText = userTitle;
+    }else {
+        document.querySelector('.modal-error').innerText = 'Please enter a task name!';
+        document.querySelector('.modal-error').style.display = 'block';
+        return;
+    }
+    if(userBody){
+        newPostBody.innerText = userBody;
+    }else {
+        document.querySelector('.modal-error').innerText = 'Please enter a task description!';
+        document.querySelector('.modal-error').style.display = 'block';
+        return;
+    }
     newPost.append(newPostTitle, newPostBody);
     newPost.classList.add('card-body');
-    console.log(document.querySelector('input[name="status"]').value)
+    newPost.addEventListener('drag', postDrag);
     document.querySelector(`.${document.querySelector('input[name="status"]:checked').value}`).children[1].append(newPost);
-    document.querySelector('#postTitle').value = '';
-    document.querySelector('#postBody').value = '';
-    postModalElement.style.display = 'none';
+    clearModal();
 });
