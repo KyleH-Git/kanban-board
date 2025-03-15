75672849e049ea522ce856b915b0f4ed66a363ce
Kyle Hayden <dahaydnator@gmail.com>
2025-03-14 19:54:24 -0500

    added modal, added eventlisteners for capturing modal values and proper container to place new element into

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 const newPostBtn = document.querySelector('.postBtn');
+const newSubmitBtn = document.querySelector('.submitBtn');
+const cancelPostBtn = document.querySelector('.cancelBtn')
+const postModalElement = document.querySelector('.post-modal');
 
-newPostBtn.addEventListener('click', (event) => {
-    console.log('to-do button clicked')
+newPostBtn.addEventListener('click', () => {
+    postModalElement.style.display = "flex";
+});
+
+cancelPostBtn.addEventListener('click', () => {
+    postModalElement.style.display = "none";
+});
+
+newSubmitBtn.addEventListener('click', () =>{
+    event.preventDefault();
+    const newPost = document.createElement('div');
+    const newPostTitle = document.createElement('div');
+    newPostTitle.innerText = document.querySelector('#postTitle').value;
+    const newPostBody = document.createElement('div');
+    newPostBody.innerText = document.querySelector('#postBody').value;
+    newPost.append(newPostTitle, newPostBody);
+    newPost.classList.add('card-body');
+    console.log(document.querySelector('input[name="status"]').value)
+    document.querySelector(`.${document.querySelector('input[name="status"]:checked').value}`).children[1].append(newPost);
+    document.querySelector('#postTitle').value = '';
+    document.querySelector('#postBody').value = '';
+    postModalElement.style.display = 'none';
 });
